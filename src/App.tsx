cc0726ef527014678de764f0bc43dd243325924e
ninomae <sotaro@soutarou-no-iMac.local>
2022-04-04 22:22:51 +0900

    remove apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,8 @@
 import React, { Suspense } from "react";
-import {
-  Link,
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import { Link, Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Transition from "./pages/Transition";
-import AutomaticBatching from "./pages/AutomaticBatching";
 import NewHooks from "./pages/NewHooks";
-
-/*
-function App() {
-  return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/tranisition">Transition</Link>
-          </li>
-          <li>
-            <Link to="/automaticbatching">AutoMaticBatching</Link>
-          </li>
-          <li>
-            <Link to="/newhooks">NewHooks</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/">
-          <Transition />
-        </Route>
-        <Route path="/tranisition">
-          <Transition />
-        </Route>
-        <Route path="/automaticbatching">
-          <AutomaticBatching />
-        </Route>
-        <Route path="/newhooks">
-          <NewHooks />
-        </Route>
-      </Routes>
-    </Router>
-  );
-}
-
-*/
 const App: React.VFC = (_) => {
   return (
     <BrowserRouter>
@@ -62,9 +15,6 @@ const App: React.VFC = (_) => {
             <Link to="/tranisition">Transition</Link>
           </li>
           <li>
-            <Link to="/automaticbatching">AutoMaticBatching</Link>
-          </li>
-          <li>
             <Link to="/newhooks">NewHooks</Link>
           </li>
         </ul>
@@ -72,7 +22,6 @@ const App: React.VFC = (_) => {
       <Routes>
         <Route index element={<Transition />} />
         <Route path="tranisition" element={<Transition />} />
-        <Route path="automaticbatching" element={<AutomaticBatching />} />
         <Route path="newhooks" element={<NewHooks />} />
       </Routes>
     </BrowserRouter>
